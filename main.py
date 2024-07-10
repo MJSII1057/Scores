9022810405a8fed75b6394698d9247591e5735c8
MJSII1057 <mjsiiisnumber1@gmail.com>
2024-07-10 02:01:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,67 @@
+p_total_runs = 0
+p_total_hits = 0
+p_total_errors = 0
+
+o_total_runs = 0
+o_total_hits = 0
+o_total_errors = 0
+
+#Inning 1
+
+p_total_runs += 0
+p_total_hits += 2
+p_total_errors += 0
+
+o_total_runs += 0
+o_total_hits += 1
+o_total_errors += 0
+
+#Inning 2
+
+p_total_runs += 2
+p_total_hits += 5
+p_total_errors += 1
+
+o_total_runs += 1
+o_total_hits += 2
+o_total_errors += 1
+
+#Inning 3
+
+
+
+#Inning 4
+
+
+
+#Inning 5
+
+
+
+#Inning 6
+
+
+
+#Inning 7
+
+
+
+#Inning 8
+
+
+
+#Inning 9
+
+
+
+#Totals
+
+if p_total_runs > o_total_runs:
+  print("The Pittsburgh Pirates have won the game!")
+else:
+  print("The Orioles have won the game!")
+print("Pirates:", p_total_runs)
+print("Orioles:", o_total_runs)
+
+
+
